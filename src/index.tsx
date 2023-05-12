08aac9ed1c819689e3367f41ec51587e65712473
Michael Pennella <michael.pennella@gmail.com>
2023-05-11 22:08:04 -0700

    Moved app render components to own subdirectory

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './component/App';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root') as Element
