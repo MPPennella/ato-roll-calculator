e55a0a6243cdb20e49875a6d2a7fbd0a91fef44e
Michael Pennella <michael.pennella@gmail.com>
2023-05-21 15:18:53 -0700

    Refactored ConditionSelectPanel to use additional subcomponent to decrease code duplication

---
+++ src/component/ConditionLabel/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './ConditionLabel.css'
+
+// Creates component consisting of a descriptive Label and Input for setting a value
+function ConditionLabel ({children, boxValue, updateValue} : {children:string, boxValue:number, updateValue:Function}) {
+    
+    // Returns only values that are zero or greater
+    // Returns same number as input if positive, zero otherwise
+    function enforcePositive(inputNum:number) : number {
+        return inputNum > 0 ? inputNum : 0
+    }
+    
+    return (
+        <div className='ConditionLabelContainer'>                
+            <label className='ConditionLabel'>{children} 
+                <input 
+                    type="number" 
+                    value={boxValue} 
+                    onChange={ (e) => updateValue( enforcePositive(Math.floor(+e.target.value)) )}
+                />
+            </label>
+        </div>
+    )
+}
+
+export default ConditionLabel
--- src/component/ConditionSelectPanel/index.tsx
+++ src/component/ConditionSelectPanel/index.tsx
@@ -1,35 +1,14 @@
 import React from 'react'
+import ConditionLabel from '../ConditionLabel'
 import './ConditionSelectPanel.css'
 
 function ConditionSelectPanel ( {atThreshold, breaks, upAT, upBr} : {atThreshold:number, breaks:number, upAT:Function, upBr:Function}) {
-    
-    // Returns only values that are zero or greater
-    // Returns same number as input if positive, zero otherwise
-    function enforcePositive(inputNum:number) : number {
-        return inputNum > 0 ? inputNum : 0
-    }
 
     return (
         <div className='ConditionSelectPanel'>
             <h2>Setup</h2>
-            <div className='ConditionLabelContainer'>                
-                <label className='ConditionLabel'>Aeon Trespass Threshold: 
-                    <input 
-                        type="number" 
-                        value={atThreshold} 
-                        onChange={ (e) => upAT( enforcePositive(Math.floor(+e.target.value)) )}
-                    />
-                </label>
-            </div>
-            <div className='ConditionLabelContainer'>
-                <label className='ConditionLabel'>Break Tokens: 
-                    <input 
-                        type="number" 
-                        value={breaks} 
-                        onChange={ (e) => upBr( enforcePositive(Math.floor(+e.target.value)) )}
-                    />
-                </label>
-            </div>
+            <ConditionLabel boxValue={atThreshold} updateValue={upAT} >Aeon Trespass Threshold:</ConditionLabel>
+            <ConditionLabel boxValue={breaks} updateValue={upBr} >Break Tokens:</ConditionLabel>
         </div>
     )
 }
