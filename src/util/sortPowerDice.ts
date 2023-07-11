38dae57a540f8fd54bf268b34e147bb9b94f0d4c
Michael Pennella <michael.pennella@gmail.com>
2023-07-10 19:25:59 -0700

    Added error-checking to catch cases of mismatching dice or unsortable dice

--- src/util/sortPowerDice.ts
+++ src/util/sortPowerDice.ts
@@ -22,43 +22,34 @@ function compareFaces( dieA:DieInfo, dieB:DieInfo ) : Boolean {
 }
 
 // Returns a sorted array in increasing value of the faces of the Power Dice provided
-// WARNING: Only works if all Power Dice are the same color
+// WARNING: Can only work if all Power Dice are the same color, cross-color comparisons are invalid
 // WARNING: Doesn't work with White Power Dice, only works with Red and Black Power Dice
-// WARNING: Currently doesn't error on previous two issues, and will return sensible-looking output
+// WARNING: In previous two cases, will return an unsorted list with logging when detected
 export default function sortPowerDice ( diceToSort : Array<DieInfo> ) : Array<DieInfo> {
     // If one or fewer items in provided array, return the input as it is already sorted
     if (diceToSort.length <= 1 ) return diceToSort
 
-    // TODO: Should include error-checking to make sure die types all match and exclude white dice
-    
-    
+    // Error-checking to make sure die types all match and exclude white dice, which are not currently sortable
+    // Get color of first die to compare rest to
+    const firstColor = diceToSort[0].color
+
+    // First check for non-matching dice
+    for ( const die of diceToSort ) {
+        // If any die does not match with the first, not all dice are of same type and return unsorted list with logging
+        if ( die.color !== firstColor ) {
+            console.log("ERROR: Not all die colors matched initial color of "+firstColor)
+            console.log("Returning unsorted list")
+            return diceToSort
+        }
+    }
+
+    // If all dice match, check if they are White
+    if ( firstColor === "white" ) {
+        console.log("ERROR: Contained White Power Dice which are unsortable at this time")
+        console.log("Returning unsorted list")
+        return diceToSort
+    }
    
-    let sortedArray = bubbleSort( diceToSort, compareFaces )
-    // let lastSwap:number
-    
-    // // Create tracker for how far we need to check on each pass (progressively shrinks as passes bubble highest values to top)
-    // // First pass must check up through the final element of the array
-    // // Keep running as long as maxIndex is second element or greater
-    // // On subsequentpasses, only need to check up to the first element of the last swap that was made in previous pass, everything beyond must already be sorted
-    // for ( let maxIndex = diceToSort.length-1 ; maxIndex >= 1 ; maxIndex = lastSwap ) {
-    //     // Track where last swap was, default 0 means no swaps, but also means only fist two elements were swapped - either way array is sorted
-    //     lastSwap = 0
-
-    //     // Create iterator for parsing array up to max index being considered on current pass
-    //     for (let x = 0; x < maxIndex; x++ ) {
-    //         // x is current index, y is next index
-    //         let y = x+1
-
-    //         // Compare and swap adjacent values that are not in lesser -> greater ordering        
-    //         if ( compareFaces( sortedArray[x].face, sortedArray[y].face )) {
-    //             let temp = sortedArray[x]
-    //             sortedArray[x] = sortedArray[y]
-    //             sortedArray[y] = temp
-    //             // Update last index swapped at
-    //             lastSwap = x
-    //         }            
-    //     }
-    // }
-
-    return sortedArray
+    // Now that we have ensured compliance, run the sort using info and our comparison function and return
+    return bubbleSort( diceToSort, compareFaces )
 }
