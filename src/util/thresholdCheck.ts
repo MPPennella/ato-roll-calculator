71c84db447f0ef82a32c6c4aedfd4aa507bb1592
Michael Pennella <michael.pennella@gmail.com>
2023-08-18 23:49:23 -0700

    Fixed typo that could cause wrong value display

--- src/util/thresholdCheck.ts
+++ src/util/thresholdCheck.ts
@@ -3,7 +3,8 @@ import type {PowerDieFace} from "../types/"
 // Returns the percentage of outcomes that meet or exceed the AT Threshold, factoring in the given number of breaks
 export default function thresholdCheck ( thresholdValue:number, breakValue:number, outcomeList:Array<PowerDieFace>) : number {
     // SPECIAL CASE: if no outcome data passed, treat as being all zeroes and return 0% successful if postive AT value, or 100% if zero (can't fail against no resistance)
-    if (outcomeList.length ===0 ) return (thresholdValue > 0) ? 30 : 100
+    console.log(outcomeList)
+    if (outcomeList.length === 0 ) return (thresholdValue > 0) ? 0 : 100
     
     // Find how many outcomes are successful given AT/Break conditions
     let initVal = 0 // Initial value
