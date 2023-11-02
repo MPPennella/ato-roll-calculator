b2c019c5d5551041c12f482dfea75a7a5ee59abf
Michael Pennella <michael.pennella@gmail.com>
2023-11-01 21:13:39 -0700

    Added buttons to select active Cycle and styled them

--- src/component/CycleSelector/index.tsx
+++ src/component/CycleSelector/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-//import './CycleSelector.css'
+import './CycleSelector.css'
 
+// Allows selection of current Cycle to curate display of relevant token types
 function CycleSelector ( {cycle, cycleUpdater} : {cycle:number, cycleUpdater:(number:number)=>void} ) {
     
-    // TODO: Update implementation to allow selection of specific cycle instead of randomizer button
     return (
         <div>
-            Cycle: {cycle}
-            <br />
-            <button onClick={(e) => cycleUpdater( Math.floor( Math.random()*3+1 ) )}>Rand</button>
+            <button className={"CycleButton" + ( (cycle===1)?" ActiveCycle":"" ) } onClick={(e) => cycleUpdater( 1 )}>I</button>
+            <button className={"CycleButton" + ( (cycle===2)?" ActiveCycle":"" ) } onClick={(e) => cycleUpdater( 2 )}>II</button>            
+            <button className={"CycleButton" + ( (cycle===3)?" ActiveCycle":"" ) } onClick={(e) => cycleUpdater( 3 )}>III</button>
+            {/* <button className={"CycleButton" + ( (cycle===4)?" ActiveCycle":"" ) } onClick={(e) => cycleUpdater( 4 )}>IV</button> */}
+            {/* <button className={"CycleButton" + ( (cycle===5)?" ActiveCycle":"" ) } onClick={(e) => cycleUpdater( 5 )}>V</button> */}
         </div>
         
     )
